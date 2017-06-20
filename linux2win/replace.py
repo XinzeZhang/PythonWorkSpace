f272386a571343023faf3cbf4c7868489dc751f6
XinzeZhang <xinze.zh@outlook.com>
2017-06-20 17:54:37 +0800

    add try-except

--- linux2win/replace.py
+++ linux2win/replace.py
@@ -7,20 +7,28 @@ import glob
 
 
 def linux2win():
-    dictionary = 'linux2win\\negotiate'
+
+    dictionary = 'linux2win/negotiate'
+    errorfiles = []
+    
     if not os.path.isdir(dictionary):
         print('input dictionary is not a dir')
         return False
 
-    for file in glob.glob(os.path.join(dictionary, '*.txt')):
-        print(file, end='\n')
-        f = open(file, 'r')
-        result = f.read()
-        result = result.replace('\n', '\r\n')
-        f.close()
-        w = open(file, 'w')
-        w.write(result)
-        w.close()
+    for file in glob.glob(os.path.join(dictionary, '*.log')):
+        try:
+            print(file, end='\n')
+            f = open(file, 'r')
+            result = f.read()
+            result = result.replace('\n', '\r\n')
+            f.close()
+            w = open(file, 'w')
+            w.write(result)
+            w.close()
+        except IOError:
+            print('error files', file)
+            errorfiles.append(file)
 
+    print('transmit completed, %d file(s) missed.' % len(errorfiles))
 
 linux2win()
