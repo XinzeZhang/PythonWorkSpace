9064aaff074e8ed865220733d10f473b215e50f1
Xinze Zhang <xinze.zh@outlook.com>
2017-07-17 21:12:50 +0800

    add commit

--- PyExcel/test.py
+++ PyExcel/test.py
@@ -22,12 +22,12 @@ def test():
     arraysteam = pe.get_array(file_name="PyExcel\\teamwork.xlsx")
     arraystarget = pe.get_array(file_name="PyExcel\\target.xlsx")
 
-    for array0 in arraysteam:
-        for arrayunit in arraystarget:
-            if(array0[1] == arrayunit[1]):
-                arrayunit.append(array0[3])
-                array0.append('Done')
-                break
+    # for array0 in arraysteam:
+    #     for arrayunit in arraystarget:
+    #         if(array0[1] == arrayunit[1]):
+    #             arrayunit.append(array0[3])
+    #             array0.append('Done')
+    #             break
 
     for array0 in arraysteam:
         print(array0)
@@ -37,8 +37,8 @@ def test():
     for arrayunit in arraystarget:
         print(arrayunit)
 
-    pe.save_as(array=arraysteam, dest_file_name="PyExcel\\teamwork1.xlsx")
-    pe.save_as(array=arraystarget, dest_file_name="PyExcel\\combine.xlsx")
+    # pe.save_as(array=arraysteam, dest_file_name="PyExcel\\teamwork1.xlsx")
+    # pe.save_as(array=arraystarget, dest_file_name="PyExcel\\combine.xlsx")
 
 
 test()
